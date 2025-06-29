72e22734ee0986b2e77858bed4982cd074b5af11
Matias <mhumpirig@unjbg.edu.pe>
2025-06-29 17:09:56 -0500

    Se agregó la función que permite agregar contactos

--- proyecto_final.cpp
+++ proyecto_final.cpp
@@ -1,30 +1,59 @@
-//agregamos las librerias a utilizar
-#include<iostream>
-#include<string>
-
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-struct contactos{
-	
-	string nombres;
-	string apellidos;
-	int edad;
-	string telefono;
-	string nacionalidad;
-	char sexo;	
-}; 
- int main (){
- 	contactos est;
- 	do {
- 		cout << "\n----- PROYECTO FINAL  -----\n";
+struct Contacto {
+    string nombres;
+    string apellidos;
+    int edad;
+    string telefono;
+    string nacionalidad;
+    char sexo;
+};
+void agregarContacto(vector<Contacto>& lista) {
+    Contacto nuevo;
+    cout << "\n--- Agregar nuevo contacto ---\n";
+    cout << "Nombres: ";
+    getline(cin, nuevo.nombres);
+    cout << "Apellidos: ";
+    getline(cin, nuevo.apellidos);
+    cout << "Edad: ";
+    cin >> nuevo.edad;
+    cin.ignore(); 
+    cout << "Teléfono: ";
+    getline(cin, nuevo.telefono);
+    cout << "Nacionalidad: ";
+    getline(cin, nuevo.nacionalidad);
+    cout << "Sexo (M/F): ";
+    cin >> nuevo.sexo;
+    cin.ignore(); 
+    lista.push_back(nuevo);
+    cout << "Contacto agregado correctamente.\n";
+}
+int main() {
+    vector<Contacto> contactos;
+    int opcion;
+    do {
+        cout << "\n----- PROYECTO FINAL -----\n";
         cout << "1. Agregar contacto\n";
         cout << "2. Buscar contacto\n";
         cout << "3. Mostrar lista de contactos\n";
         cout << "4. Editar contactos existentes\n";
         cout << "5. Eliminar contacto\n";
         cout << "6. Salir\n";
-        cin.ignore();
-	 return 0;
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+        cin.ignore();  // Limpiar salto de línea después del número
+
+        switch (opcion) {
+            case 1:
+                agregarContacto(contactos);
+                break;
+        }
+    } while (opcion != 6);
+    return 0;
 }
+
  
 
  
