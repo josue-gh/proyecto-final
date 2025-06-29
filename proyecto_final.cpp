c2b3a666e30f07d940ea6c1d396e07892b8498b7
Matias <mhumpirig@unjbg.edu.pe>
2025-06-29 17:06:13 -0500

    Se agrego el menu de opciones a seleccionar

--- proyecto_final.cpp
+++ proyecto_final.cpp
@@ -3,4 +3,28 @@
 #include<string>
 
 using namespace std;
+struct contactos{
+	
+	string nombres;
+	string apellidos;
+	int edad;
+	string telefono;
+	string nacionalidad;
+	char sexo;	
+}; 
+ int main (){
+ 	contactos est;
+ 	do {
+ 		cout << "\n----- PROYECTO FINAL  -----\n";
+        cout << "1. Agregar contacto\n";
+        cout << "2. Buscar contacto\n";
+        cout << "3. Mostrar lista de contactos\n";
+        cout << "4. Editar contactos existentes\n";
+        cout << "5. Eliminar contacto\n";
+        cout << "6. Salir\n";
+        cin.ignore();
+	 return 0;
+}
+ 
+
  
